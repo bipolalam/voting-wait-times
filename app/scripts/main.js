596b0f1e0b2ebfe70d0f3c893a952c7b095bbc4e
Charles Julian Knight <charles@rabidaudio.com>
2016-03-05 14:29:57 -0500

    cleanup

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,4 @@
 
-// super secure....
-
-
 // { "timestamp": "2016-03-05T00:00:00", "precinct": "1", "waittime": 2, "reporterid": "e3f99640d60577f72086b54087423593", "state": "GA"}
 function addEstimate(precinctnum, state, email, waittime, callback){
   $.ajax({
@@ -28,12 +25,6 @@ function getTimes(precinctnum, state, callback){
     dataType: 'json'
   }).then(callback);
 }
-
-
-
-// function getReporterID(email){
-//   return md5(email);
-// }
 
 
 $(document).ready(function(){
