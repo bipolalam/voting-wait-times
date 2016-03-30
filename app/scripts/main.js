9024234f63a58355d66deb94d45ee3fef452aec2
Bipol Alam <balam@ispatechnology.com>
2016-03-30 12:22:19 -0400

    add a smooth scroll from anchors

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -109,5 +109,18 @@ $(document).ready(function(){
     sendWait(wait);
   });
 
+  $('a[href^="#"]').on('click',function (e) {
+    e.preventDefault();
+
+    var target = this.hash;
+    var $target = $(target);
+
+    $('html, body').stop().animate({
+        'scrollTop': $target.offset().top
+    }, 600, 'swing', function () {
+        window.location.hash = target;
+    });
+  });
+
   // $('.btn-panic')
 });
